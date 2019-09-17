9edf14f5e30bf05e9d394a5d71fac83acc3599f8
Poulpy <paul.repain@yahoo.com>
2019-09-17 03:29:55 -0400

    Update to v 0.1.19 + remove fro now ui components, lib not ready yet

--- game_of_life.v
+++ game_of_life.v
@@ -8,6 +8,7 @@ import gg
 import glfw
 import strings
 import rand
+import ui
 
 #flag -lncurses
 #include "curses.h"
@@ -23,6 +24,7 @@ const (
 
     LivingCell = 'O'
     DeadCell   = ' '
+    WindowTitle = 'Game of Life'
     LivingCellColor = gx.Red
     DeadCellColor   = gx.White
 
@@ -39,10 +41,17 @@ const (
 /* Structs */
 
 struct Point {
-mut:
+    mut:
     x int
     y int
 }
+
+/*struct Context
+{
+    window *ui.Window
+    mut:
+    grid []array_int
+}*/
 
 /* Main */
 
@@ -66,11 +75,20 @@ fn main() {
         cli_game(mut grid, mut living_cells)
     }
     else {
+        /*context := &Context {}
+        context.window := ui.new_window(ui.WinCfg {
+            width: WindowWidth
+            height: WindowHeight
+            title: WindowTitle
+            draw_fn: draw
+            ptr: context
+        })
+        for {
+            ui.wait_events()
+        }*/
         gui_game(mut grid, mut living_cells)
     }
 }
-
-
 fn has_console_arg() bool {
     return '-c' in os.args
 }
@@ -95,7 +113,7 @@ fn gui_game(grid mut []array_int, living_cells mut []Point)
         height: WindowHeight
         use_ortho: true
         create_window: true
-        window_title: 'Game of Life'
+        window_title: WindowTitle
         window_user_ptr: game
     })
     game.window.set_user_ptr(game)
@@ -374,14 +392,14 @@ fn init_grid() []array_int {
     mut a := []array_int
 
     for i := 0; i != MaxHeight; i++ {
-        a << [0; MaxWidth]
+        a << [0].repeat(MaxWidth)
     }
 
     return a
 }
 
 fn add_cells(living_cells []Point) []array_int {
-    grid := init_grid()
+    mut grid := init_grid()
     mut x := 0
     mut y := 0
 
@@ -491,4 +509,51 @@ fn print_cells(g mut gg.GG, grid []array_int)
     }
     g.render()
 }
+
+/*
+fn draw(c *Context)
+{
+    mut x := 0
+    mut y := 0
+    mut cell_size := 0
+    mut cell_color := gx.White
+
+    upper_left   := Point { x: HorizontalBorder - space, y: VerticalBorder - space }
+    upper_right  := Point { x: WindowWidth - HorizontalBorder + space, y: VerticalBorder - space }
+    bottom_right := Point { x: WindowWidth - HorizontalBorder + space, y: WindowHeight - VerticalBorder + space }
+    bottom_left  := Point { x: HorizontalBorder - space, y: WindowHeight - VerticalBorder + space }
+
+    gx.draw_line_c(upper_left.x, upper_left.y, upper_right.x, upper_right.y, c)
+    gx.draw_line_c(upper_left.x, upper_left.y, bottom_left.x, bottom_left.y, c)
+    gx.draw_line_c(upper_right.x, upper_right.y, bottom_right.x, bottom_right.y, c)
+    gx.draw_line_c(bottom_left.x, bottom_left.y, bottom_right.x, bottom_right.y, c)
+
+
+    for l, line in c.grid
+    {
+        for c, cell in line
+        {
+            x = c * BlockSize + HorizontalBorder
+            y = l * BlockSize + HorizontalBorder
+
+            if cell == 0
+            {
+                x--
+                y--
+                cell_size = BlockSize + 1
+                cell_color = DeadCellColor
+            }
+            else
+            {
+                x++
+                y++
+                cell_size = BlockSize - 1
+                cell_color = LivingCellColor
+            }
+
+            gx.draw_rect(x, y, cell_size, cell_size, cell_color)
+        }
+    }
+}
+*/
 
